5c446f00db3c22bd056230e88fd391d41e3200de
Charles <charlesvonkalm@gmail.com>
2026-03-08 18:50:03 +1100

    seed databases for participants, messages, conversations

--- app/data/seed/route.ts
+++ app/data/seed/route.ts
@@ -15,7 +15,8 @@ async function seedUsers() {
       name VARCHAR(255) NOT NULL UNIQUE,
       password TEXT NOT NULL,
       admin BOOL NOT NULL DEFAULT FALSE,
-      image_url bytea
+      image_url bytea,
+      created_at TIMESTAMPTZ DEFAULT NOW()
     );
   `;
 
@@ -33,10 +34,51 @@ async function seedUsers() {
   return insertedUsers;
 }
 
+async function seedMessages() {
+  await sql`
+    CREATE TABLE IF NOT EXISTS messages (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      conversation_id UUID NOT NULL REFERENCES conversations(id) ON DELETE CASCADE,
+      sender_id UUID REFERENCES users(id) ON DELETE SET NULL,
+
+      content TEXT NOT NULL,
+      created_at TIMESTAMPTZ DEFAULT NOW()
+    );
+  `;
+}
+
+async function seedParticipants() {
+  await sql`
+    CREATE TABLE IF NOT EXISTS participants (
+      conversation_id UUID NOT NULL REFERENCES conversations(id) ON DELETE CASCADE,
+      user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+      last_read_at TIMESTAMPTZ DEFAULT NOW(),
+      joined_at TIMESTAMPTZ DEFAULT NOW(),
+
+      PRIMARY KEY (conversation_id, user_id)
+    );
+  `;
+}
+
+async function seedConversations() {
+  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`
+
+  await sql`
+    CREATE TABLE IF NOT EXISTS conversations (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      title VARCHAR(60),
+      created_at TIMESTAMPTZ DEFAULT NOW()
+    );
+  `;
+}
+
 export async function GET() {
   try {
     const result = await sql.begin((sql) => [
       seedUsers(),
+      seedParticipants(),
+      seedConversations(),
+      seedMessages(),
     ]);
 
     return Response.json({message: 'database seeded!'});
