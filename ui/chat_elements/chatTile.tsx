0b67793e253a10bac59d6522a4fa252b3a80cf9f
Charles <charlesvonkalm@gmail.com>
2026-03-13 17:56:19 +1100

    fix chat tiles not appearing in the right spot

--- ui/chat_elements/chatTile.tsx
+++ ui/chat_elements/chatTile.tsx
@@ -5,17 +5,18 @@ import { friendListItem } from "@/lib/definitions";
 
 export default function ChatTile(props: friendListItem) {
   return (
-    <div className="bg-graphite border-shadow-grey p-2 h-15 flex items-center drop-shadow-black hover:bg-lavender hover:text-barbie-pink hover:drop-shadow-lg transition-all duration-200">
-      <FaceSmileIcon className="shrink-0 w-10"/>
-      
-      <Link
-        href={`/chat/${props.chatid}`}
-        className="px-3 flex flex-col overflow-hidden"
-      >
-        <p className={`${bitcount.className} antialiased`}>@{props.chatname}</p>
-        <p className="truncate text-sm opacity-60">{props.last_message}</p>
-      </Link>
-
+    <div className="bg-graphite border-shadow-grey p-2 h-15 flex items-center justify-between drop-shadow-black hover:bg-lavender hover:text-barbie-pink hover:drop-shadow-lg transition-all duration-200">
+      <div className="flex-1 items-center flex flex-row">
+        <FaceSmileIcon className="shrink-0 w-10"/>
+        
+        <Link
+          href={`/chat/${props.chatid}`}
+          className="px-3 flex flex-col overflow-hidden"
+        >
+          <p className={`${bitcount.className} antialiased`}>@{props.chatname}</p>
+          <p className="truncate text-sm opacity-60">{props.last_message}</p>
+        </Link>
+      </div>
       {props.unread_count !== 0 ? (
         <div className="bg-barbie-pink mr-1 w-6 h-6 aspect-square rounded-full flex items-center justify-center">
           <p className={`${bitcount.className} antialiased text-white transform -translate-y-[-1.2px] translate-x-[.5px]`}>
